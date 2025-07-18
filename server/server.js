bfbacd9eeb6fc9857134dcb8cea2314c7371789d
Hkonjeti95 <hemanthkonjeti79@gmail.com>
2025-07-18 14:27:10 +1000

    Backend update

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users',      userRoutes);
 app.use('/api/timesheets', timesheetRoutes);
 app.use('/api/admin',      adminRoutes);
